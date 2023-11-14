2618fa9edbb4a7dd7a830a5cf8fc61c70e6e0c29
GinRay8 <136614240+GinRay8@users.noreply.github.com>
2023-11-14 23:44:13 +0200

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+gallery.style.listStyleType = 'none';
+
+function galleryMarkup(items) {
+    return items.map((item) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+           <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+    </li>`
+    ).join("");
+};
+
+const addGalleryMarkup = galleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("afterbegin", addGalleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionsDelayTime: 250});
